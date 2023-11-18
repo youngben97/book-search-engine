e8b558715265eeed68bdc58a765cf52e195d5cd8
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-17 21:09:20 -0700

    call query

---
+++ client/src/utils/queries.js
@@ -0,0 +1,22 @@
+// Important for useQuery: We bring in gql from the @apollo/client library to allow us to parse queries (and mutations) as template literals
+import { gql } from '@apollo/client';
+
+// Important for useQuery: Each query we'd like to be able to perform gets exported out of our queries.js utility
+export const GET_ME = gql`
+    query me($meId: ID!) {
+  me(id: $meId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      image
+      link
+      title
+    }
+  }
+}
+`;
