ce10776ec5d14208a9838840284e80161b466e3a
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-17 21:12:59 -0700

    changed proxy to graphql

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,11 +7,14 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    //Here we're establishing a relationship between our two development servers.
+    // We are pointing our Vite client-side development server to proxy API requests to our server-side Node server at port 3001.
+    // Without this line, API calls would attempt to fallback and query for data from the current domain: localhost:3000
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
       }
     }
   }
