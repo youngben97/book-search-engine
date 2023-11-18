2db3626274601a469e2c6660c3cf06a69a81ebed
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-17 21:07:02 -0700

    added autherror, return req if !token, return req instead of next

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,10 +6,15 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +22,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -29,7 +35,9 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    // next();
+    // return the request object so it can be passed to the resolver as `context`
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
