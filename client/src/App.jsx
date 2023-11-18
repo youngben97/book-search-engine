79dedac57a3ea435f5fc812e1248f1db5ebab1c5
GreatGaspargoo <b.c.young.97@gmail.com>
2023-11-17 21:12:40 -0700

    wrapped app in apollo provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css';
+// Important for API Consumption: To enable interaction with our GraphQL API on the front end, we utilize these tools to develop the client-side behavior
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Important for API Consumption: Create an instance of the ApolloClient class and specify the endpoint of your GraphQL API (e.g., 'http://localhost:3001')—the proxy set up in the previous activity facilitates this. 
+// We also instantiate a new InMemoryCache class that automatically caches queried data, enhancing performance.
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+      </ApolloProvider>
   );
 }
 
